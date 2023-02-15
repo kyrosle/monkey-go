14cc794b6981418038aa0d78ac3105eb13838d96
kyrosle <le@90e.com>
2023-02-15 22:40:23 +0800

    add gin server support

--- main.go
+++ main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"lexer-parser/repl"
-	"os"
-	"os/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
-	fmt.Printf("Typing Commands\n")
-	repl.Start(os.Stdin, os.Stdout)
+	r := gin.Default()
+	r.GET("/code", func(c *gin.Context) {
+		buf := make([]byte, 1024)
+		n, _ := c.Request.Body.Read(buf)
+		c.Request.Body = ioutil.NopCloser(bytes.NewReader(buf[:n]))
+		raw_code := string(buf[0:n])
+		fmt.Println("body: ", raw_code)
+		ret := repl.StartHandle(raw_code)
+		fmt.Println("Response: ", ret)
+		c.JSON(http.StatusOK, ret)
+	})
+	r.Run(":8888") 
 }
+
+// func CommandUsed() {
+// user, err := user.Current()
+// if err != nil {
+// 	panic(err)
+// }
+
+// fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+// fmt.Printf("Typing Commands\n")
+// repl.Start(os.Stdin, os.Stdout)
+// }
--- repl/repl.go
+++ repl/repl.go
@@ -8,19 +8,20 @@ import (
 	"lexer-parser/lexer"
 	"lexer-parser/object"
 	"lexer-parser/parser"
+	"strings"
 )
 
 const PROMPT = ">> "
 
-func Start(in io.Reader, out io.Writer) {
-	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
-
-	builtinFns := `
+const builtinFns = `
 let map = fn(arr, f) { let iter = fn(arr, accumulated) { if (len(arr) == 0) { accumulated } else { iter(rest(arr), push(accumulated, f(first(arr)))); } }; iter(arr, []); };
 let reduce = fn(arr, initial, f) { let iter = fn(arr, result) { if (len(arr) == 0) { result } else { iter(rest(arr), f(result, first(arr))); } }; iter(arr, initial); };
 let sum = fn(arr) { reduce(arr, 0, fn(initial, el) { initial + el }); };
 	`
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	l := lexer.New(builtinFns)
 	p := parser.New(l)
@@ -49,6 +50,43 @@ let sum = fn(arr) { reduce(arr, 0, fn(initial, el) { initial + el }); };
 		if evaluator != nil {
 			io.WriteString(out, evaluator.Inspect())
 			io.WriteString(out, "\n")
+		}
+	}
+}
+
+func StartHandle(raw string) string {
+	l := lexer.New(builtinFns)
+	p := parser.New(l)
+	program := p.ParseProgram()
+	env := object.NewEnvironment()
+	evaluator.Eval(program, env)
+
+	raw = strings.ReplaceAll(raw, "\n", "")
+	sr := strings.NewReader(raw)
+	scanner := bufio.NewScanner(sr)
+	buf := new(strings.Builder)
+	buf.Grow(len(raw))
+
+	for {
+		scanned := scanner.Scan()
+		if !scanned {
+			return buf.String()
+		}
+		line := scanner.Text()
+		l = lexer.New(line)
+		p = parser.New(l)
+
+		program = p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(buf, p.Errors())
+			continue
+		}
+
+		evaluator := evaluator.Eval(program, env)
+		if evaluator != nil {
+			io.WriteString(buf, evaluator.Inspect())
+			io.WriteString(buf, "\n")
 		}
 	}
 }

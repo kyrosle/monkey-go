af11dde6f18b9b19cce39e028288910d2b3d7d07
kyrosle <le@90e.com>
2023-02-16 23:09:13 +0800

    support return error if handing is timeout

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"lexer-parser/repl"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,11 +19,27 @@ func main() {
 		c.Request.Body = ioutil.NopCloser(bytes.NewReader(buf[:n]))
 		raw_code := string(buf[0:n])
 		fmt.Println("body: ", raw_code)
-		ret := repl.StartHandle(raw_code)
-		fmt.Println("Response: ", ret)
-		c.JSON(http.StatusOK, ret)
+
+		channel := make(chan string, 1)
+		wait_channel := make(chan bool, 1)
+
+		go func() {
+			channel <- repl.StartHandle(raw_code)
+			wait_channel <- true
+		}()
+
+		select {
+		case <-wait_channel:
+			ret := <-channel
+			fmt.Println("Response: ", ret)
+			c.JSON(http.StatusOK, ret)
+		case <- time.After(time.Duration(5) * time.Second):
+			fmt.Println("Handle Timeout")
+			c.JSON(http.StatusLocked, "")
+		}
+
 	})
-	r.Run(":8888") 
+	r.Run(":8888")
 }
 
 // func CommandUsed() {
